edbd373c443d206d1afdd2fc416dbe09f28b23b4
= <soufian.kamma@gmail.com>
2023-11-16 06:15:45 -0800

    Handling value returned from environment

--- environ.c
+++ environ.c
@@ -2,29 +2,37 @@
 
 /**
  * _getenv - gets the value of an environ variable
- * @name: environ string to search with
+ * @name_env: environ string to search with
  *
  * Return: value of environ
  */
-char *_getenv(char *name)
+char *_getenv(char *name_env)
 {
-	char *tmp = NULL, *env = NULL, *path = NULL;
+	char *tmp = NULL, *env = NULL, *path = NULL,
+	     *tmp_name = NULL, *name = NULL, *tmp_env = NULL;
 	int i;
 
+	tmp_name = _strdup(name_env);
+	name = strtok(tmp_name, "=");
 	for (i = 0; environ[i]; i++)
 	{
-		tmp = _strdup(environ[i]);
-		path = starts_with(tmp, name);
-		if (path)
+		tmp_env = _strdup(environ[i]);
+		tmp = strtok(tmp_env, "=");
+		if (_strcmp(tmp, name) == 0)
 		{
-			path++;
+			if (tmp)
+				path = strtok(NULL, "=");
+
 			env = _strdup(path);
-			free(tmp), tmp = NULL;
+			free(tmp_name), tmp_name = NULL;
+			free(tmp_env), tmp_env = NULL;
 			return (env);
 		}
 
-		free(tmp), tmp = NULL;
+		free(tmp_env), tmp_env = NULL;
 	}
+
+	free(tmp_name), tmp_name = NULL;
 
 	return (NULL);
 }
